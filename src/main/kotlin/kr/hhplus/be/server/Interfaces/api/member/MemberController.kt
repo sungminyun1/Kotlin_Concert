7ec5818e6f886047d238945141799c652314085e
sungminyun1 <sungmin.yun@nflux.co.kr>
2025-01-02 13:33:20 +0900

    feat: 포인트 조회 mock api 에러 추가
    에러 response 추가

--- src/main/kotlin/kr/hhplus/be/server/Interfaces/api/member/MemberController.kt
+++ src/main/kotlin/kr/hhplus/be/server/Interfaces/api/member/MemberController.kt
@@ -1,6 +1,7 @@
 package kr.hhplus.be.server.Interfaces.api.member
 
 import kr.hhplus.be.server.Interfaces.api.member.dto.PointResponse
+import kr.hhplus.be.server.common.exceptions.HplusNotfoundException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,8 +14,9 @@ class MemberController {
 
    @GetMapping("/{userId}/point")
    fun getMemberPoint(
-       @PathVariable("userId") userName: String,
+       @PathVariable("userId") userId: String,
    ): PointResponse {
+       if(userId == "error") throw HplusNotfoundException("에러 응답 예시")
        return PointResponse("userId", 1000, LocalDateTime.now())
    }
 }
